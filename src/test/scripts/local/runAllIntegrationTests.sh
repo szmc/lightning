035b8ed1ed8a98c19b51c11b7c9af18b2aef90f5
automatictester <theautomatictester@gmail.com>
2015-08-01 11:39:38 +0100

    Amend tests and int test exec script

--- src/test/scripts/local/runAllIntegrationTests.sh
+++ src/test/scripts/local/runAllIntegrationTests.sh
@@ -2,6 +2,8 @@
 
 echo -e 'Run all Integration Tests\n'
 
+mvn clean compile assembly:single
+
 ./src/test/scripts/console/output/report_10_0.sh
 ./src/test/scripts/console/output/verify_1_1_1.sh
 ./src/test/scripts/console/output/verify_3_0_0.sh
